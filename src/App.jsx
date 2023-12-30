78861ce0d96628eed90f23c3be199ad53a526f1e
Gregory Gerard <gregory@flatchr.io>
2023-12-30 14:26:50 +0100

    feat: add newproject & input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import ProjectsSideBar from "./components/ProjectsSideBar";
+import NewProject from "./components/NewProject";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSideBar/>
+      <NewProject/>
     </main>
   );
 }
