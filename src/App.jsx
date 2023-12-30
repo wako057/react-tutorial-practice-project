df7e984941004c9d5d600b2554fc3b95d6efd922
Gregory Gerard <gregory@flatchr.io>
2023-12-30 15:33:18 +0100

    feat: add input/button/noproject - manage state / forward ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
 import ProjectsSideBar from "./components/ProjectsSideBar";
 import NewProject from "./components/NewProject";
+import {useState} from "react";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    console.log('projectData: ', projectData);
+    const newProject = {
+      ...projectData,
+      id: Math.random()
+    };
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        projects: [ ...prevState.projects, newProject]
+      };
+    })
+  }
+
+  console.log('projectsState: ', projectsState);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content= <NewProject onAdd={handleAddProject}/>
+  } else if (projectsState.selectedProjectId === undefined ) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar/>
-      <NewProject/>
+      <ProjectsSideBar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
